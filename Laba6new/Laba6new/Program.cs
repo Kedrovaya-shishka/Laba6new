14e81ae283fd5f8f3a7ca708255ae84375880bfa
Kedrovaya-shishka <Misha12082008@icloud.com>
2026-03-06 10:53:15 +0200

    Add files via upload

---
+++ Laba6new/Laba6new/Program.cs
@@ -0,0 +1,25 @@
+﻿//Laba 6
+//Number 1
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int a = N;
+int b = 0;
+while (a > 0)
+{
+    a /= 10;
+    b++;
+}
+int[] Func = new int[b];
+MyFunc getFunc = (int x) => x % 10;
+a = N;
+for (int i = b - 1; i >= 0; i--)
+{
+    Func[i] = getFunc(a);
+    a = a / 10;
+}
+Console.WriteLine("Массив цифр:");
+foreach (int d in Func)
+{
+    Console.Write(d + " ");
+}
+delegate int MyFunc(int x);
